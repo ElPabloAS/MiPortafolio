70a43e4c824ff4c61e1e6c937691300bc5fd5a38
ElPabloAS <pabloalvaro5@gmail.com>
2024-12-14 21:48:28 +0100

    1

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".",  // Cambia la raíz del proyecto a "src"
+  build: {
+    outDir: "../docs",  // Los archivos construidos irán a la carpeta "docs"
+  },
+  base: '/portfolio',
+});
